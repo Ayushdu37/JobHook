e5333ce2bd0b71a3014039e075f512fba3f09b46
Ayush Dubey <ayushdu37@gmail.com>
2025-06-20 00:13:55 +0530

    Initial commit with frontend

---
+++ src/main/java/com/jobportal/service/UserServiceImpl.java
@@ -0,0 +1,35 @@
+package com.jobportal.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.jobportal.dto.UserDTO;
+import com.jobportal.entity.User;
+import com.jobportal.exception.JobPortalException;
+import com.jobportal.repository.UserRepository;
+import com.jobportal.utility.Utilities;
+
+@Service(value="userService")
+public class UserServiceImpl implements UserService{
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+	
+	@Override
+	public UserDTO registerUser(UserDTO userDTO) throws JobPortalException {
+		Optional<User> optional = userRepository.findByEmail(userDTO.getEmail());
+		if(optional.isPresent())throw new JobPortalException("USER_FOUND");
+		userDTO.setId(Utilities.getNextSequence("users"));
+		userDTO.setPassword(passwordEncoder.encode(userDTO.getPassword()));
+		User user= userDTO.toEntity();
+		user=userRepository.save(user);
+		return user.toDTO();
+	}
+
+}
